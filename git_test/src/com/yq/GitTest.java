27b9d2e95873e5c0e8895bc1c905b942693f2a10
hlq <hlq@yq.com>
2019-05-29 16:41:17 +0800

    添加2222222222222

--- git_test/src/com/yq/GitTest.java
+++ git_test/src/com/yq/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test...");
         System.out.println("111111111111111111111");
+        System.out.println("2222222222222222222222");
     }
     public void aa(){
         System.out.println("aaaaaaaaaaaaaaa");
