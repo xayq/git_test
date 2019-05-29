5625e12cd1bfcc2f251ee2f649f33283c5fdbb29
hlq <hlq@yq.com>
2019-05-29 16:29:08 +0800

    将本地项目添加到git

---
+++ git_test/src/com/yq/GitTest.java
@@ -0,0 +1,8 @@
+package com.yq;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git Test...");
+        System.out.println("111111111111111111111");
+    }
+}
